39f4089f7a33826e6b4025527c84c23756da19a4
Mohamed Amr Taha <61754682+MohamedAmrTaha@users.noreply.github.com>
2025-05-18 15:21:00 +0300

    handling content overflow in dashboard page

--- Blog-dashboard/frontend/src/Components/Dashboard.tsx
+++ Blog-dashboard/frontend/src/Components/Dashboard.tsx
@@ -83,7 +83,7 @@ const Dashboard: React.FC = () => {
               <h3 className="text-lg font-semibold text-gray-800">
                 {post.title}
               </h3>
-              <p className="text-gray-600">{post.body.slice(0, 100)}...</p>
+              <p className="text-gray-600 break-words">{post.body}</p>
               <button
                 className="mt-2 px-3 py-1 bg-blue-600 text-white rounded hover:bg-blue-700 cursor-pointer"
                 onClick={() => navigate(`/posts/${post.id}`)}
